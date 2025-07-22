a1e734c14ee4425fea00dc1d43630413f0883649
Ferry <lopezdelcidferrydejesus@gmail.com>
2025-07-21 20:49:09 -0600

    Final

--- verificador_fecha_valida.py
+++ verificador_fecha_valida.py
@@ -1,5 +1,5 @@
 day = int(input("Ingrese el día: "))
-month = int(input("Ingrese el año: "))
+month = int(input("Ingrese el mes: "))
 year = int(input("Ingrese el año: "))
 valid_date = True
 if month < 1 or month > 12:
@@ -20,8 +20,16 @@ if valid_date:
             valid_date = False
 if valid_date:
     if month < 3:
-        month += 12
-        year -= 1
-    century = year // 100
-    year_on_century = year % 100
-    zeller = (day + ((13 * (month + 1)) // 5 + year_on_century + (year_on_century // 4) + (century // 4) + 5 * century) % 7
+        month_z = month + 12
+        year_z = year - 1
+    else:
+        month_z = month
+        year_z = year
+    century = year_z // 100
+    year_on_century = year_z % 100
+    zeller = (day + (13 * (month_z + 1)) // 5 + year_on_century + (year_on_century // 4) + (century // 4) + 5 * century) % 7
+    days_week = ["Sábado", "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+    day_week = days_week[zeller]
+    print(f"La fecha {day}/{month}/{year} fue un {day_week}.")
+else:
+    print("Fecha inválida.")
